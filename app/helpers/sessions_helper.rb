38b0a98b213af32341f1642d99b34a3ad553ff1e
Renato da Silva <renato.katia@gmail.com>
2017-05-29 16:12:23 +0000

    Ajuste no mailer

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,7 +17,7 @@ module SessionsHelper
     user == current_user
   end
 
-  # Returns the user corresponding to the remember token cookie.
+  # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -29,7 +29,7 @@ module SessionsHelper
       end
     end
   end
-
+  
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
